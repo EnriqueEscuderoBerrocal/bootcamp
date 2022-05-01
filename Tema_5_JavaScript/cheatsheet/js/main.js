d44c9cdc00d66d9b54ff6f5181615e21d448ab89
Enrique Escudero <74680851+EnriqueEscuderoBerrocal@users.noreply.github.com>
2022-05-01 20:49:35 +0200

    javaScript tema 33 variables Datos primitivos

--- Tema_5_JavaScript/cheatsheet/js/main.js
+++ Tema_5_JavaScript/cheatsheet/js/main.js
@@ -54,7 +54,7 @@ let suma = texto +dia;
 console.log(suma);
 
  // ------------------- Tipos de datos -----------------
-
+// datos primitivos
  console.clear();
 
  let str = String("hola tronco");
@@ -65,3 +65,55 @@ console.log(suma);
  let num2 = Number(5);
  console.log(typeof num, typeof num2);
  
+ let bool = Boolean(true);
+ let bool2 =false;
+ console.log(typeof bool, typeof bool2);
+
+ let undef;
+console.log(typeof undef);
+
+let selectedColor =null;  // Limpiar una variable
+console.log(selectedColor, typeof selectedColor);
+
+// Objetos
+let array1 = Array(1,2,3,4);
+let array2 = [1,2,3];
+console.log(array1,array2);
+console.log(typeof array1, typeof array2);
+
+console.log(typeof new Date("2022-05-01"));
+console.log(typeof new Error("Mensaje de Error 001"));
+
+// --------------------- Strings ------------------------
+
+str ="Hola Futuro";
+console.log(str , str.length); // nos dice el numero de letras
+console.log(str.toUpperCase());  // convierte el texto en mayuscula
+console.log(str.indexOf("Futuro")); // nos dice donde empieza la palabra futuro dentro del String
+console.log(str.indexOf("trololo")); // nos data como valor -1 al no existir dentro del String
+console.log("La última ocurrencia de 'u' esta en el indice : ", str.lastIndexOf("u"));
+
+console.log(str.substring(0,4)); // nos da como valor los caracteres empezando en 0 y terminando en 4
+
+console.log(str.concat(" y Pasado")); //concatena Strings
+
+console.log(str.replace("Futuro", "Manteca"));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+console.log(`texto que me de la gana ${str} porque me da la gana. ${str2}`); // solo para ES6 "String Template",recuerda, el de la tecla del `` sombrerito
+
+// --- Parsing -----
+
+let number2 =Number ("1000");
+console.log(number2, typeof number2);
+
+ number2 =Number ("fdfdgfd"); // NaN = no es un numero , no genera error
+console.log(number2, typeof number2);
+number2 = 1000;
+number2 += "1000"; // entiende que es una concatenación.
+console.log(number2, typeof number2);
+
+
+
+
